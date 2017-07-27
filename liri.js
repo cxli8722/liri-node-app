51a8abc73af5aa043d5f99eda7845ed091d87002
Li Chen <cxli@email.unc.edu>
2017-07-27 16:27:40 -0400

    updated

--- liri.js
+++ liri.js
@@ -29,16 +29,53 @@ var keys=require('./keys.js')
 
 
 
+
 var client = new Twitter(
  keys.twitterKeys 
 );
+//paramters for search topic and count 
+var params={ 
+  q: 'cake since:2017-07-1', 
+  count: 10
+   };
 
 
-client.get('favorites/list', function(error, tweets, response) {
-  if(error) throw error;
- //console.log("tweets:" + JSON.stringify(tweets, null, 2));
+client.get('search/tweets', params, DataReturn);
+  function DataReturn(err, data, response) {
 
- tweets.map(function(t){console.log(t.text)}) // The favorites. 
-  //console.log(response);  // Raw response object. 
-  console.log(tweets.length)
+ 
+var tweets= data.statuses
+tweets.map(function(t){console.log(t.text)}) // loop through the tweets  
+ 
+console.log("top ten tweets on cake: " +tweets.length)
+
+// for (var i=0; i <tweets.length; i++)
+// console.log(tweets[i].text)
+};
+
+
+
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: "b8b59d7c98f042ef8e9ff638c3a48e1b",
+  secret: "f32142a935504faf9e7a65f2245ec012"
 });
+ 
+spotify
+  .search({ type: 'track', query: 'I want it that way' })
+  .then(function(data) {
+
+    for(var i = 0; i < data.tracks.items.length; i++){
+        var songInfo = data.tracks.items[i];
+  
+   
+  }
+   console.log("Artist: " + songInfo.artists[0].name);
+   console.log('Song Title: ' + songInfo.name)
+    console.log("Preview URL: " + songInfo.preview_url);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
